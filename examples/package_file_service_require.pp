51b98b67307145224079e244cb2f16e6d34012e2
ryan <rcorales@traxiontech.net>
2020-05-04 18:13:14 +0800

    mysql06

--- examples/package_file_service_require.pp
+++ examples/package_file_service_require.pp
@@ -3,7 +3,7 @@ package { 'mysql-server':
 }
 
 file { '/etc/mysql/mysql.cnf':
-  source  => '/root/devopspuppet/examples/files/mysql.cnf',
+#  source  => '/root/devopspuppet/examples/files/mysql.cnf',
   notify  => Service['mysql'],
   require => Package['mysql-server'],
 }
