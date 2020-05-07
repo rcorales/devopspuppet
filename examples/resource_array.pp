9189aff406aef0ee634448e265b3d758b902eebf
ryan <rcorales@traxiontech.net>
2020-05-07 12:41:53 +0800

    php array

--- examples/resource_array.pp
+++ examples/resource_array.pp
@@ -1,12 +1,12 @@
 $dependencies = [
-  'php7.3-cgi',
-  'php7.3-cli',
-  'php7.3-common',
-  'php7.3-gd',
-  'php7.3-json',
-  'php7.3-mcrypt',
-  'php7.3-mysql',
-  'php7.3-soap',
+  'php7.2-cgi',
+  'php7.2-cli',
+  'php7.2-common',
+  'php7.2-gd',
+  'php7.2-json',
+  'php7.2-mcrypt',
+  'php7.2-mysql',
+  'php7.2-soap',
 ]
 
 package { $dependencies:
